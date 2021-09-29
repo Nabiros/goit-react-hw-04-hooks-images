b811fc27a0512a098a043c8bf5f3d245b6a06bef
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-29 20:50:48 +0300

    refactoring code, used hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -14,6 +14,10 @@ export const ImageGallery = ({ images, onSelect }) => {
 }
 
 ImageGallery.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.object),
+    images: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            image: PropTypes.object,
+        })),
     onSelect: PropTypes.func,
 };
